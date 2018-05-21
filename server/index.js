44a2ed2075756788db59a8b0bf032c3b30b6c8f8
Samseppiol <j_hage@live.com>
2018-05-21 20:22:23 +1000

    setup oauth with google, have response coming back but it is not redirecting after the initial permission.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const config = require('./config/config.json')
 const app = express();
 
 
-
+passport.use(new GoogleStrategy({
+        clientID: config.googleClientID,
+        clientSecret: config.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, cb) => {
+        console.log('Access token:', accessToken)
+        console.log('Refresh Token:', refreshToken)
+        console.log('Profile:', profile)
+    }
+))
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+    res.send('Root page')
+})
+app.get('/success', (req, res) => {
+    res.send('Successful Login!')
+})
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile']}))
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/'}), (req, res) => {
+        res.redirect('/success')
+    }
+)
 
 
 const PORT = process.env.PORT || 4000;
